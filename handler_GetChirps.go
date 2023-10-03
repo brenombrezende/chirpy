87ab8ddc36718bc08ad6e8d7ee850cb723b000e6
brenombrezende <brenombrezende@gmail.com>
2023-10-03 20:31:04 -0300

    feat/ added final features such as sorting, query per author id and etc

--- handler_GetChirps.go
+++ handler_GetChirps.go
@@ -3,18 +3,45 @@ package main
 import (
 	"net/http"
 	"sort"
+	"strconv"
+
+	"github.com/brenombrezende/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+
+	s := r.URL.Query().Get("author_id")
 
 	chirps, err := cfg.DB.GetChirps()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps")
 	}
 
-	sort.Slice(chirps, func(i, j int) bool {
-		return chirps[i].Id < chirps[j].Id
-	})
+	if s != "" {
+		qId, _ := strconv.Atoi(s)
+		filteredChirps := []database.Chirp{}
+		for i := range chirps {
+			if chirps[i].Author_Id == qId {
+				filteredChirps = append(filteredChirps, chirps[i])
+			}
+
+		}
+		chirps = filteredChirps
+	}
+
+	sorting := r.URL.Query().Get("sort")
+	if sorting == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].Id > chirps[j].Id
+		})
+
+	} else {
+
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].Id < chirps[j].Id
+		})
+
+	}
 
 	respondWithJSON(w, 200, chirps)
 }
